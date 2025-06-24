970d9b2323939b2ff83899ee4ca30bf54e76d96f
MagdalenaStranska <stranska.magdalena@gmail.com>
2025-06-24 20:29:55 +0200

    Rozdeleno na funkcni komponenty

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css';
+
+export const Footer = () => {
+    return(
+      <div className="container">
+        <div className="footer__content">
+          Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+        </div>
+      </div>
+          
+
+    )
+}
